da5001f97cb61535a1ce922be89c151e34252d95
DenysPolitiuk <denys.politiuk@protonmail.com>
2019-10-05 21:18:12 -0400

    Added test and small adj on main file to test it

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const app = require('express')();
+const app = require("express")();
 
 const nameRoute = (req, res) => {
-	const name = req.query.name;
-	res.send(`Hello ${name || 'stranger'}!`);
-}
+  const name = req.query.name;
+  res.send(`Hello ${name || "stranger"}!`);
+};
 
-app.get('/hello', nameRoute)
+app.get("/hello", nameRoute);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Running at ${port}`));
+if (!module.parent) {
+  app.listen(port, () => console.log(`Running at ${port}`));
+}
+
+module.exports = app;
---
+++ index_test.js
@@ -0,0 +1,33 @@
+const api = require("./index.js");
+const server = require("supertest")(api);
+const should = require("should");
+
+describe("unit test", () => {
+  it("/ should return 404", done => {
+    server.get("/").expect(404, done);
+  });
+
+  it("hello should return 200", done => {
+    server.get("/hello").expect(200, done);
+  });
+
+  it("hello should return default text", done => {
+    server
+      .get("/hello")
+      .expect(200)
+      .end((err, res) => {
+        res.text.should.equal("Hello stranger!");
+      });
+    done();
+  });
+
+  it("hello should return name", done => {
+    server
+      .get("/hello?name=Denys")
+      .expect(200)
+      .end((err, res) => {
+        res.text.should.equal("Hello Denys!");
+      });
+    done();
+  });
+});
