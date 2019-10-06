920cfe45a4ea989c86d66908a77e0e81cd11804f
DenysPolitiuk <denys.politiuk@protonmail.com>
2019-10-06 15:04:22 -0400

    Added deployment to k8s cluster
    Also added other k8s files used to configure cluster

--- docker/docker-registry-deploy.sh
+++ docker/docker-registry-deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && \
-docker build -t denyspolitiuk/simple-api . && \
-docker push denyspolitiuk/simple-api
+set -e
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t denyspolitiuk/simple-api:latest .
+docker push denyspolitiuk/simple-api:latest
---
+++ kubernetes-deployment/kubernetes-deploy.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+set -e
+
+echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+./kubectl --kubeconfig=/dev/null --server=$KUBERNETES_SERVER --certificate-authority=cert.crt --token=$KUBERNETES_TOKEN \
+	apply -f ./kubernetes/deployment.yaml
