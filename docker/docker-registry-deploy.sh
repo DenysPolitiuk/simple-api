65903827143b90f75b06b151ce21db6c649a00ae
DenysPolitiuk <denys.politiuk@protonmail.com>
2019-10-06 15:24:10 -0400

    Added id to image build and deploy for it

--- docker/docker-registry-deploy.sh
+++ docker/docker-registry-deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t denyspolitiuk/simple-api:latest .
-docker push denyspolitiuk/simple-api:latest
+docker build -t denyspolitiuk/simple-api:$TRAVIS_BUILD_ID .
+docker push denyspolitiuk/simple-api:$TRAVIS_BUILD_ID
--- kubernetes-deployment/kubernetes-deploy.sh
+++ kubernetes-deployment/kubernetes-deploy.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 set -e
+COMMIT=$TRAVIS_BUILD_ID
+export COMMIT=$COMMIT
+
+envsubst <./kubernetes/deployment.yaml > ./kubernetes/deployment.yaml.out
+mv ./kubernetes/deployment.yaml.out ./kubernetes/deployment.yaml
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 ./kubectl --kubeconfig=/dev/null --server=$KUBERNETES_SERVER --certificate-authority=cert.crt --token=$KUBERNETES_TOKEN \
